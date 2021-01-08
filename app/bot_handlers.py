9e89f261806fa73f504fea439d72c60a7033038e
gaspero <gasp-gasp@mail.ru>
2021-01-09 02:54:20 +0300

    app.json update, v0.1

--- app/bot_handlers.py
+++ app/bot_handlers.py
@@ -1,6 +1,11 @@
 from app import bot
 
 
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, 'Hey there! Try texting anything :)')
+
+
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
     bot.reply_to(message, message.text)
